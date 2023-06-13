68389d37e870a9a44fd8eba6a64e48d472d59cb1
Iakov Salikov <salikovpro@gmail.com>
2023-06-13 16:44:08 +0100

    fix: unused envs

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,6 @@ export default {
     OPENAI_KEY: process.env.OPENAI_KEY,
     PORT: process.env.PORT,
     REDIS_URL: process.env.REDIS_URL,
-    URL: process.env.URL,
     POSTGRES_DB: process.env.POSTGRES_DB,
     POSTGRES_HOST: process.env.POSTGRES_HOST,
     POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
